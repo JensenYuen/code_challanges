553056e3c17ab578e8b8b8dde23428a312d254ae
jensen yuen <jensenyuen@outlook.com>
2022-03-12 17:52:04 +0800

    solved part two

--- advent_of_code/day_2/dive.rb
+++ advent_of_code/day_2/dive.rb
@@ -20,7 +20,7 @@ def dive(inputs)
       directions[:verital] += value
     end
   end
-  directions[:verital] * directions[:horizontal]
+  (directions[:verital] * directions[:horizontal]).abs
 end
 
 test_inputs = "forward 5
@@ -33,7 +33,33 @@ forward 2".split(/\n/)
 inputs = File.open('./dive_input.txt').read.split(/\n/)
 
 # Each input would need to be split again for direction and distance
-p dive(test_inputs)
-p dive(inputs)
+# Test input should return true (150)
+# p dive(test_inputs) == 150
+# p dive(inputs)
 
-def dive_part_two; end
+def dive_part_two(inputs)
+  directions = {
+    horizontal: 0,
+    depth: 0,
+    aim: 0
+  }
+  inputs.each do |input|
+    direction_arr = input.split(' ')
+    key = direction_arr[0].to_s.to_sym
+    value = direction_arr[1].to_i
+    case key
+    when :up
+      directions[:aim] -= value
+    when :down
+      directions[:aim] += value
+    else
+      directions[:horizontal] += value
+      directions[:depth] += value * directions[:aim]
+    end
+  end
+  (directions[:depth] * directions[:horizontal]).abs
+end
+
+# Test input should return trun (900)
+p dive_part_two(test_inputs) == 900
+p dive_part_two(inputs)
